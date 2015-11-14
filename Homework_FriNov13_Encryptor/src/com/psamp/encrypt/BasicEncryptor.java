ffc6bad1837b6f93596cf8f4d554666cd2c8bd2c
Princess Sampson <ps@kingonomics.com>
2015-11-14 18:23:05 -0500

    finished hw

--- Homework_FriNov13_Encryptor/src/com/psamp/encrypt/BasicEncryptor.java
+++ Homework_FriNov13_Encryptor/src/com/psamp/encrypt/BasicEncryptor.java
@@ -1,11 +1,15 @@
 package com.psamp.encrypt;
 
 class BasicEncryptor extends Encryptor {
-
-	@Override
+	
 	String encrypt(String str) {
 		StringBuffer buffer = new StringBuffer(str);
-		return this.getClass() + " | " + buffer.reverse().toString().toUpperCase();
+		return super.header() + buffer.reverse().toString().toUpperCase();
 	}
 
+	@Override
+	String decrypt(String str) {
+		StringBuffer buffer = new StringBuffer(str);
+		return super.header() + "decrypt | " + buffer.reverse().toString().toLowerCase();
+	}
 }
